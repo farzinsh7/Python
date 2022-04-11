befdde39a8305e4148bb4b716a0e7a9b66c78cb4
farzinsh7 <farzinnater@gmail.com>
2022-04-11 15:48:22 +0430

    List-1

---
+++ chapter 3 (lists)/lists.py
@@ -0,0 +1,11 @@
+names = ['ali', 'hassan', 'sina']
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# -----------------------------------
+# print(f"Hello MR.{names[0]}")
+# print(f"Hello MR.{names[1]}")
+# print(f"Hello MR.{names[2]}")
+# -----------------------------------
+cars = ['benz', 'tesla', 'BMW']
+print(f"{names[1]} is my best friend and he has lots of cars such as {cars[2]}")

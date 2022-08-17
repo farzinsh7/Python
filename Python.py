5ff487f096c2180ca59540e9d2694a317f74e2bc
farzinsh7 <farzinnater@gmail.com>
2022-08-17 23:52:37 +0430

    Week_1

---
+++ Python.py
@@ -0,0 +1,14 @@
+# data = "hello there my name is farzin shams you can send me a message if you want here is my email:me@farzinshams.com now guess my website?"
+# find_email = data.find("@")
+# print(find_email)
+# find_space = data.find(" ", find_email)
+# print(find_space)
+# website = f"You website is: www.{data[find_email+1: find_space]}"
+# print(website)
+# for letter in 'banana':
+#     print(letter)
+# text = "X-DSPAM-Confidence:    0.8475"
+# find_0 = text.find("0")
+# find_num = text[find_0:]
+# fnum = float(find_num)
+# print(fnum)

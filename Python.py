c6547c96bfe358bd471244083598cefb9ed4defe
farzinsh7 <farzinnater@gmail.com>
2022-08-26 16:19:50 +0430

    review

--- Python.py
+++ Python.py
@@ -1,24 +1,25 @@
-fname = input("Enter your file: ")
-oname = open(fname)
-myDict = dict()
-myList = list()
-for line in oname:
-    line = line.rstrip()
-    if line.startswith("From "):
-        line = line.split()
-        day = line[2]
-        myList.append(day)
-for item in myList:
-    myDict[item] = myDict.get(item, 0)+1
-lst = list()
-for key, value in myDict.items():
-    lst.append((key, value))
-lst = sorted(lst, reverse=True)
-for k, v in lst:
-    if k == "Thu":
-        k = "Thursday"
-    elif k == "Sat":
-        k = "Saturday"
-    elif k == "Fri":
-        k = "Friday"
-    print(f"You worked {v} Day's in {k}")
+# fname = input("Enter your file: ")
+# oname = open(fname)
+# myDict = dict()
+# myList = list()
+# for line in oname:
+#     line = line.rstrip()
+#     if line.startswith("From "):
+#         line = line.split()
+#         day = line[2]
+#         myList.append(day)
+# for item in myList:
+#     myDict[item] = myDict.get(item, 0)+1
+# lst = list()
+# for key, value in myDict.items():
+#     lst.append((key, value))
+# lst = sorted(lst, reverse=True)
+# for k, v in lst:
+#     if k == "Thu":
+#         k = "Thursday"
+#     elif k == "Sat":
+#         k = "Saturday"
+#     elif k == "Fri":
+#         k = "Friday"
+#     print(f"You worked {v} Day's in {k}")
+# -------------------------

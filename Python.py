4e182a78a324d41f5db6236bd2910b9a8dccf171
farzinsh7 <farzinnater@gmail.com>
2022-09-12 16:24:00 +0430

    Iterate

--- Python.py
+++ Python.py
@@ -1,5 +1,12 @@
 # to see Problems : CTRL + SHIFT + M
 # T see Command Pallete : CTRL + SHIFT + P
 # to run in terminal : CTRL + ALT + N
-x = 1
-print(id(x))
+# Built in functions: https://docs.python.org/3/library/functions.html
+# Math funvtion: https://docs.python.org/3/library/math.html
+age = 30
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Eligible"
+# -----OR-----
+message = "Eligible" if age >= 18 else "Not Eligible"

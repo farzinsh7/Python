f96f6e072f9fe79dc9a3de99932254ea16c5ace8
farzinsh7 <farzinnater@gmail.com>
2022-10-30 10:20:03 +0330

    new

--- Python.py
+++ Python.py
@@ -174,29 +174,30 @@
 #     print(error)
 
 # -------------------Cost-of-raising=exceptions-------------------
-from timeit import timeit
-code1 = """
-def calculate_xfactor(age):
-    if age <= 0:
-        raise ValueError("Age cannot be 0 or less.")
-    return 10/age
+# from timeit import timeit
+# code1 = """
+# def calculate_xfactor(age):
+#     if age <= 0:
+#         raise ValueError("Age cannot be 0 or less.")
+#     return 10/age
 
 
-try:
-    calculate_xfactor(-1)
-except ValueError as error:
-    pass
-    """
+# try:
+#     calculate_xfactor(-1)
+# except ValueError as error:
+#     pass
+#     """
 
-code2 = """
-def calculate_xfactor(age):
-    if age <= 0:
-        return None
-    return 10/age
+# code2 = """
+# def calculate_xfactor(age):
+#     if age <= 0:
+#         return None
+#     return 10/age
 
-xfactor = calculate_xfactor(-1)
-if xfactor == None:
-    pass
-    """
-print("First code=", timeit(code1, number=10000))
-print("First code=", timeit(code2, number=10000))
+# xfactor = calculate_xfactor(-1)
+# if xfactor == None:
+#     pass
+#     """
+# print("First code=", timeit(code1, number=10000))
+# print("First code=", timeit(code2, number=10000))
+# -------------------Classes-------------------
